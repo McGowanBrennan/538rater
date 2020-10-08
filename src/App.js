7ae0547961fd3277ba62b62e761ef61c17dea983
Brennan McGowan <btm5jg@virginia.edu>
2020-10-08 13:08:38 -0400

    Organization update
    - Added actual file structure

--- src/App.js
+++ src/App.js
@@ -1,33 +1,15 @@
 import React from 'react';
 
-import GetMembers from "./GetMembers"
+import LandingPage from "./Components/LandingPage/LandingPage.js"
 import "./sass/homeScreen.scss"
+
 
 class App extends React.Component{
   constructor(){
     super()
     this.state = {
-      items: [],
-      loading: false,
-      zip: "",
-      members: [],
-      leaveRating: false,
-      viewRating: false
+     
     }
-    this.handleClick1 = this.handleClick1.bind(this)
-    this.handleClick2 = this.handleClick2.bind(this)
-  }
-
-  handleClick1(){
-    this.setState({
-      leaveRating: true
-    })
-  }
-
-  handleClick2(){
-    this.setState({
-      viewRating: true
-    })
   }
 
   componentDidMount(){
@@ -48,54 +30,10 @@ class App extends React.Component{
 
 
   render(){
-    
-
-    if(this.state.leaveRating){
-      return(
-      <GetMembers />
-      )
-    }
-
-    if(this.state.viewRating){
-      return(
-        <GetMembers />
-        )
-    }
-
-    return(
-    
-        
-        <div>
-          <div className = "title">
-  <code>538rater.com</code>
-  </div>
-<div class="container">
-  <div class="row">
-    <div class="col-sm">
-    <button class="button" onClick = {this.handleClick1}>
-	<code>Leave    rating </code>
-	<div class="button__horizontal"></div>
-	<div class="button__vertical"></div>
-</button>
-    </div>
-    <div class="col-sm">
-      
-    </div>
-    <div class="col-sm">
-    <button class="button" onClick = {this.handleClick2}>
-<code>View RATINGS</code>
-	<div class="button__horizontal"></div>
-	<div class="button__vertical"></div>
-</button>
-    </div>
-  </div>
-</div>
-
-</div>
-
-
-
-     
+    return(    
+        <div>   
+          <LandingPage />
+        </div>
     )
   }
 }
---
+++ src/Components/LandingPage/LandingPage.js
@@ -0,0 +1,79 @@
+import React from "react"
+
+import GetMembers from "./Componenets/SearchReps/GetMembers.js"
+import "./Sass/homeScreen.scss"
+
+class LandingPage extends React.Component{
+  constructor(){
+    super()
+    this.state = {
+      items: [],
+      loading: false,
+      zip: "",
+      members: [],
+      leaveRating: false,
+      viewRating: false
+    }
+    this.handleClick1 = this.handleClick1.bind(this)
+    this.handleClick2 = this.handleClick2.bind(this)
+  }
+
+  handleClick1(){
+    this.setState({
+      leaveRating: true
+    })
+  }
+
+  handleClick2(){
+    this.setState({
+      viewRating: true
+    })
+  }
+
+  render(){
+    
+    if(this.state.leaveRating){
+      return(
+      <GetMembers />
+      )
+    }
+
+    if(this.state.viewRating){
+      return(
+        <GetMembers />
+        )
+    }
+
+    return( 
+        <div>
+          <div className = "title">
+                <code>538rater.com</code>
+                </div>
+                <div class="container">
+                <div class="row">
+                    <div class="col-sm">
+                    <button class="button" onClick = {this.handleClick1}>
+                    <code>Leave    rating </code>
+                    <div class="button__horizontal"></div>
+                    <div class="button__vertical"></div>
+                </button>
+                    </div>
+                    <div class="col-sm">
+                    
+                    </div>
+                    <div class="col-sm">
+                    <button class="button" onClick = {this.handleClick2}>
+                <code>View RATINGS</code>
+                    <div class="button__horizontal"></div>
+                    <div class="button__vertical"></div>
+                </button>
+                    </div>
+                </div>
+          </div>
+
+        </div>
+    )
+  }
+}
+
+export default LandingPage;
